a4eac7d6afafb6b0fe3a98624a036a920b420d45
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2022-03-20 03:02:57 +0100

    Fixed travel to flower logic.

--- FlourishProject/Assets/Scripts/BeeAgentScript.cs
+++ FlourishProject/Assets/Scripts/BeeAgentScript.cs
@@ -9,12 +9,15 @@ public class BeeAgentScript : MonoBehaviour
 {
     //Components
     [SerializeField] private GameObject heightRegulator;
+    [SerializeField] private Animator animator;
 
     private NavMeshAgent agent;
     private GameObject[] listOfFlowers;
     private GameObject targetFlower = null;
 
+
     //Variables
+    private bool isOnFlower = false;
     private float timeBetweenTarget = 0f;
 
 
@@ -29,32 +32,55 @@ public class BeeAgentScript : MonoBehaviour
     //Update
     private void Update()
     {
+        //If not already in a flower, travel to it
+        if (!isOnFlower) TravelToFlower();
+    }
 
+
+    //Land in the flower, recollect pollen and search for another flower
+    private IEnumerator LandOnFlower()
+    {
+        isOnFlower = true;
+
+        //Set the animation
+        animator.SetBool("OnFlower", true);
+
+        //Wait a random time before leaving
+        yield return new WaitForSeconds(2);
+
+        //Leave the flower
+        SetNewDestination();
+
+        animator.SetBool("OnFlower", false);
+
+        isOnFlower = false;
+    }
+
+
+    //Travel to flower AI
+    private void TravelToFlower()
+    {
         //If there isn't a destination flower, set and new one and return
         if (targetFlower == null)
         {
             SetNewDestination();
             return;
         }
+        else //If the flower has not disappeared, calculate remaining position time
+        {
+            //Do a tween between the heightRegulator Y and the target flower Y
+            //Debug.Log(timeBetweenTarget);
+            timeBetweenTarget = agent.remainingDistance / agent.speed;
+            heightRegulator.transform.DOMoveY(targetFlower.transform.position.y, timeBetweenTarget * 5);
+        }
 
         //If the agent is in the target position,
-        //it doesn't have path or it's speed is 0, set a new destination
+        //it doesn't have path or it's speed is 0, land in the flower
         if (agent.remainingDistance <= agent.stoppingDistance)
         {
             //Using sqrMagnitude instead of magnitude increases performance
-            if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f) SetNewDestination();
+            if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f) StartCoroutine(LandOnFlower());
         }
-
-        //Move the agent to the target flower, if the flower has not disappeared
-        if (targetFlower != null) agent.SetDestination(targetFlower.transform.position);
-
-        //Vector3 positionToMove = new Vector3(transform.position.x, targetFlower.transform.position.y, transform.position.z);
-        //heightRegulator.transform.DOMove(positionToMove, 4);
-
-        //Do a tween between the heightRegulator Y and the target flower Y
-        timeBetweenTarget = agent.remainingDistance / agent.speed;
-        heightRegulator.transform.DOMoveY(targetFlower.transform.position.y, timeBetweenTarget * 5);
-        Debug.Log(timeBetweenTarget);
     }
 
 
@@ -67,6 +93,9 @@ public class BeeAgentScript : MonoBehaviour
         //Get a random flower and set it as the target
         int randomDestination = Random.Range(0, listOfFlowers.Length);
         targetFlower = listOfFlowers[randomDestination];
+
+        //Set the destination
+        agent.SetDestination(targetFlower.transform.position);
     }
 
 }
