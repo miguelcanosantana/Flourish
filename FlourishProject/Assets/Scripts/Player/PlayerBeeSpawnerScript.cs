488494402054aded1583514ab6463e28b046cefb
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2022-04-10 21:46:01 +0200

    Cleaned and added backpack.

---
+++ FlourishProject/Assets/Scripts/Player/PlayerBeeSpawnerScript.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBeeSpawnerScript : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject backPackObject;
+
+    //Variables
+    private List<GameObject> nearFlowers = new List<GameObject>();
+    private List<GameObject> nearBees = new List<GameObject>();
+    private bool canCheck = true;
+
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canCheck) StartCoroutine(CheckFlowersAndBeesAround());
+    }
+
+
+    //Check the flowers and the bees around, spawn necessary bees
+    private IEnumerator CheckFlowersAndBeesAround()
+    {
+        canCheck = false;
+
+        yield return new WaitForSeconds(1f);
+
+        canCheck = true;
+    }
+
+
+    //On trigger enter
+    private void OnTriggerEnter(Collider collider)
+    {
+        //Add the flower to the list if it's near
+        if (collider.gameObject.tag == "Flower") nearFlowers.Add(collider.gameObject);
+
+        //Add the bee to the list if it's near
+        if (collider.gameObject.tag == "Bee") nearBees.Add(collider.gameObject);
+    }
+
+
+    //On trigger exit
+    private void OnTriggerExit(Collider collider)
+    {
+        //Remove the flower from the list if it's far
+        if (collider.gameObject.tag == "Flower") nearFlowers.Remove(collider.gameObject);
+
+        //Remove the bee from the list if it's far
+        if (collider.gameObject.tag == "Bee") nearBees.Remove(collider.gameObject);
+    }
+}
