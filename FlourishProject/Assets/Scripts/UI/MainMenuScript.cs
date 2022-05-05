9c2be340af52074129ba5799caa1e2207becce42
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2022-05-05 14:54:04 +0200

    Added menu things and empty save.

---
+++ FlourishProject/Assets/Scripts/UI/MainMenuScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    //Event => OnNewClick
+    public void OnNewClick()
+    {
+
+    }
+
+
+    //Event => OnContinueClick
+    public void OnContinueClick()
+    {
+
+    }
+
+
+    //Event => OnConfigClick
+    public void OnConfigClick()
+    {
+
+    }
+
+
+    //Event => OnExitQuit, exit the application
+    public void OnExitQuit()
+    {
+        Application.Quit();
+    }
+}
