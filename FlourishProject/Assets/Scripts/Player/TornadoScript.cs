ceb134db1571306f3e550fa2d085915480f8db34
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2022-05-10 07:11:58 +0200

    Update TornadoScript.cs

--- FlourishProject/Assets/Scripts/Player/TornadoScript.cs
+++ FlourishProject/Assets/Scripts/Player/TornadoScript.cs
@@ -11,8 +11,8 @@ public class TornadoScript : MonoBehaviour
     //When objects enter the tornado add them to the list
     private void OnTriggerEnter(Collider collider)
     {
-        //If the object is not already on the list and it's a bee (Suck Bee layer), add it
-        if (!objectsBeingSucked.Contains(collider.gameObject) && collider.gameObject.layer == 11) 
+        //If the object is not already on the list and it's a bee, add it
+        if (!objectsBeingSucked.Contains(collider.gameObject) && collider.gameObject.CompareTag("Bee"))
         {
             objectsBeingSucked.Add(collider.gameObject);
         }
@@ -22,8 +22,8 @@ public class TornadoScript : MonoBehaviour
     //When objects exit the tornado remove them from the list
     private void OnTriggerExit(Collider collider)
     {
-        //If the object is in the list and it's a bee (Suck Bee layer), remove it
-        if (objectsBeingSucked.Contains(collider.gameObject) && collider.gameObject.layer == 11)
+        //If the object is in the list and it's a bee, remove it
+        if (objectsBeingSucked.Contains(collider.gameObject) && collider.gameObject.CompareTag("Bee"))
         {
             objectsBeingSucked.Remove(collider.gameObject);
         }
